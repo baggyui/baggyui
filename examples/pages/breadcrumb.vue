c0199e33257b629f16471e9921820fa05dd56f57
cobish <cobish110@gmail.com>
2017-06-01 14:11:38 +0800

    add breadcrumb demo

--- examples/pages/breadcrumb.vue
+++ examples/pages/breadcrumb.vue
@@ -1,23 +1,33 @@
 <template>
   <div>
     <p class="title">面包屑</p>
-    <v-breadcrumb>
-      <v-breadcrumb-item to="/">首页</v-breadcrumb-item>
-      <v-breadcrumb-item :to="{ path: '/button' }">按钮</v-breadcrumb-item>
-      <v-breadcrumb-item to="/table">表格</v-breadcrumb-item>
-      <v-breadcrumb-item>面包屑</v-breadcrumb-item>
-    </v-breadcrumb>
+    <div class="crumb-box">
+      <v-breadcrumb>
+        <v-breadcrumb-item to="/">首页</v-breadcrumb-item>
+        <v-breadcrumb-item :to="{ path: '/button' }">按钮</v-breadcrumb-item>
+        <v-breadcrumb-item to="/table">表格</v-breadcrumb-item>
+        <v-breadcrumb-item>面包屑</v-breadcrumb-item>
+      </v-breadcrumb>
+    </div>
 
     <p class="title">自定义分隔符</p>
-    <v-breadcrumb separator=">">
-      <v-breadcrumb-item to="/">首页</v-breadcrumb-item>
-      <v-breadcrumb-item :to="{ path: '/button' }">按钮</v-breadcrumb-item>
-      <v-breadcrumb-item to="/table">表格</v-breadcrumb-item>
-      <v-breadcrumb-item>面包屑</v-breadcrumb-item>
-    </v-breadcrumb>
+    <div class="crumb-box">
+      <v-breadcrumb separator=">">
+        <v-breadcrumb-item to="/">首页</v-breadcrumb-item>
+        <v-breadcrumb-item :to="{ path: '/button' }">按钮</v-breadcrumb-item>
+        <v-breadcrumb-item to="/table">表格</v-breadcrumb-item>
+        <v-breadcrumb-item>面包屑</v-breadcrumb-item>
+      </v-breadcrumb>
+    </div>
   </div>
 </template>
 
 <script>
   export default {};
 </script>
+
+<style lang="sass" scoped>
+  .crumb-box {
+    padding: 10px 0;
+  }
+</style>
