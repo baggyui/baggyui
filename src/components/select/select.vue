df508e08f1f947da15d1252847153561964bb64c
cobish <cobish110@gmail.com>
2017-06-30 18:37:37 +0800

    优化 select position

--- src/components/select/select.vue
+++ src/components/select/select.vue
@@ -7,7 +7,11 @@
     </div>
 
     <transition name="v-select-zoom">
-      <div v-show="visible" :class="prefixCls + '-dropdown'">
+      <div
+        ref="dropdown"
+        v-show="visible"
+        :class="prefixCls + '-dropdown'"
+        :style="style">
         <ul :class="prefixCls + '-list'">
           <slot></slot>
         </ul>
@@ -18,6 +22,7 @@
 
 <script>
   import clickoutside from '../../utils/clickoutside';
+  import { getOffset } from '../../utils/fn';
 
   const prefixCls = 'v-select';
 
@@ -39,7 +44,8 @@
       return {
         prefixCls,
         visible: false,
-        selected: ''
+        selected: '',
+        style: {}
       };
     },
 
@@ -71,6 +77,7 @@
 
       visible(val) {
         if (val) {
+          this.setPosition();
           this.$emit('on-focus');
         } else {
           this.$emit('on-blur');
@@ -107,6 +114,52 @@
             child.selected = false;
           }
         });
+      },
+
+      setPosition() {
+        if (this.visible) {
+          const selectHeight = 34;
+          const p = getOffset(this.$el);
+
+          this.style = {
+            top: p.top + selectHeight + 'px',
+            left: `${p.left}px`,
+            width: `${p.right - p.left}px`,
+            maxHeight: `${this.maxHeight}px`,
+          };
+        }
+      },
+
+      init() {
+        document.body.appendChild(this.$refs.dropdown);
+
+        // 监听窗口改动
+        window.addEventListener('resize', this.setPosition);
+
+        // 监听滚动条改动
+        let parent = this.$el.parentNode;
+        while (parent !== document.body) {
+          parent.addEventListener('scroll', this.setPosition);
+          parent = parent.parentNode;
+        }
+      },
+
+      destroy() {
+        // 移除窗口改动
+        window.removeEventListener('resize', this.setPosition);
+
+        // 移除滚动条改动
+        let parent = this.$el.parentNode;
+        while (parent !== document.body) {
+          try {
+            parent.removeEventListener('scroll', this.setPosition);
+            parent = parent.parentNode;
+          } catch (e) {
+            parent = document.body;
+          }
+        }
+
+        document.body.removeChild(this.$refs.dropdown);
       }
     },
 
@@ -114,6 +167,12 @@
       this.$on('on-select-change', (value, label) => {
         this.select(value, label);
       });
+
+      this.init();
+    },
+
+    beforeDestroy() {
+      this.destroy();
     }
   };
 </script>
