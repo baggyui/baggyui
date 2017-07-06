15c98256aaf1ef8cbcf8717a4e86b711058328bf
cobish <cobish110@gmail.com>
2017-07-06 16:13:03 +0800

    add unit test

---
+++ test/unit/specs/icon.spec.js
@@ -0,0 +1,44 @@
+import Vue from 'vue';
+import Icon from '../../../src/components/icon';
+
+/**
+ * 获取生成的vm
+ *
+ * @param {Object} Component 组件
+ * @param {Object} propsData props数据
+ * @return {Object} vue实例
+ */
+function getRenderedVm(Component, propsData) {
+  const Ctor = Vue.extend(Component);
+  const vm = new Ctor({ propsData }).$mount();
+  return vm;
+}
+
+
+describe('Icon', () => {
+  it('props type', () => {
+    const vm = getRenderedVm(Icon, {
+      type: 'arrow-shrink'
+    });
+
+    expect(vm.$el.classList.contains('v-icon-arrow-shrink')).to.true;
+  });
+
+  it('props color', () => {
+    const vm = getRenderedVm(Icon, {
+      type: 'arrow-shrink',
+      color: 'rgb(255, 255, 255)'
+    });
+
+    expect(vm.$el.style.color).to.equal('rgb(255, 255, 255)');
+  });
+
+  it('props size', () => {
+    const vm = getRenderedVm(Icon, {
+      type: 'arrow-shrink',
+      size: '20'
+    });
+
+    expect(vm.$el.style.fontSize).to.equal('20px');
+  });
+});
