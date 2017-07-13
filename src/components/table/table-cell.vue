dac1ebc59407f0d8c3a06ebb126d036aac0e0b7f
xiaojia_huang <xiaojia_huang@kingdee.com>
2017-07-13 18:35:08 +0800

    优化 Table 组件 render 函数

--- src/components/table/table-cell.vue
+++ src/components/table/table-cell.vue
@@ -1,21 +1,33 @@
 <template>
   <div class="cell" ref="cell">
-    <template v-if="column.type === 'selection'">
-      <v-checkbox
-        v-model="cellChecked"
-        @on-change="handleChange"></v-checkbox>
+    <Cell
+      v-if="column.render"
+      :row="row"
+      :index="index"
+      :render="column.render"></Cell>
+    <template v-else>
+      <template v-if="column.type === 'selection'">
+        <v-checkbox
+          v-model="cellChecked"
+          @on-change="handleChange"></v-checkbox>
+      </template>
+      <template v-else>{{ row[column.key] }}</template>
     </template>
-    <template v-else>{{ row[column.key] }}</template>
   </div>
 </template>
 
 <script>
   import Vue from 'vue';
+  import Cell from './expand';
 
   const prefixCls = 'v-table-cell';
 
   export default {
     name: prefixCls,
+
+    components: {
+      Cell
+    },
 
     props: {
       data: Array,
@@ -36,49 +48,10 @@
         if (this.column.type === 'selection') {
           this.cellChecked = val[this.index]._checked;
         }
-
-        this.compile();
-      }
-    },
-
-    methods: {
-      compile() {
-        if (!this.column.render) {
-          return;
-        }
-
-        const component = new Vue({
-          functional: true,
-          render: (h) => {
-            return this.column.render(h, {
-              self: this,
-              row: this.row,
-              column: this.column,
-              index: this.index
-            });
-          }
-        });
-
-        // 清空，因为会重复渲染，到时参考 iView 优化
-        this.$el.innerHTML = '';
-
-        const Cell = component.$mount();
-        this.$refs.cell.appendChild(Cell.$el);
-      },
-
-      handleChange(val) {
-        let obj = {
-          index: this.index,
-          checked: val
-        };
-
-        this.$emit('select-cell', obj);
       }
     },
 
     mounted() {
-      this.compile();
-
       if (this.checked && this.column.type === 'selection') {
         this.handleChange(this.checked);
       }
